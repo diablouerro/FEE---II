e28d4aa90ce7a489557028818552f231fd116709
vinayakdhar <vinayakdhar003@gmail.com>
2024-08-30 12:44:12 +0530

    30-08-2024

---
+++ Classes/lec29/array_methods.js
@@ -0,0 +1,31 @@
+// let arr = [1,2,3,4,5];
+
+// // mutubale:
+// arr[2] = 5;
+// console.log(arr);
+
+// // length:
+// console.log(arr.length);
+
+// 2d array:
+// let arr = [[1,2],[2,3],[3,4]];
+// console.log(arr[0][1]);
+
+
+// ---- Methods ----
+
+// 1. push:
+// let fruits = ['apple', 'bananas', 'grapes'];
+// console.log(fruits.push('pear'));
+// console.log(fruits);
+
+// 2. pop:
+// let fruits = ['apple', 'bananas', 'grapes'];
+// console.log(fruits.pop());
+// console.log(fruits);
+
+// 3. shift:
+// let fruits = ['apple', 'bananas', 'grapes'];
+// console.log(fruits.shift()); // removes 1st element by default
+// console.log(fruits.unshift("guava"));
+// console.log(fruits);
---
+++ Classes/lec29/string_methods.js
@@ -0,0 +1,40 @@
+// 1. trim method: 
+// let msg = "     hello world      ";
+// console.log(msg.trim());
+
+// 2. toUpper case:
+// let txt = "have a nice day";
+// console.log(txt.toUpperCase());
+
+// 3. toLower case:
+// let txt = "have a nice day";
+// console.log(txt.toLowercase());
+
+// 4. indexOf:
+// let msg = "hello how are you";
+// console.log(msg.indexOf());
+
+// 5. mutubale or non-mutable:
+// let txt = "have a nice day";
+// txt[3] = 't';
+// console.log(txt);
+
+// 6. slices: (index-start, index-end - 1)
+// let txt = "have a nice day";
+// console.log(txt.slice(2));
+// console.log(txt.slice(2,11));
+// console.log(txt.slice(2));
+
+// 7. replace:
+// let txt = "have a nice day";
+// console.log(txt.replace('day', 'sunset'));
+
+// 8. repeat:
+// let txt = "143\n";
+// console.log(txt.repeat(20));
+
+// 9. string template literals:
+let fname = "Vinayak";
+let lname = "Dhar";
+    // console.log("Mr." + fname + " " + lname);
+console.log(`Mr. ${fname} ${lname}`);
